2a018f6878f96ce0e5678cfd931d323f44d009a8
Echo <echo-the-coder@tuta.io>
2022-05-09 09:26:09 -0400

    update

--- tasks/join.ts
+++ tasks/join.ts
@@ -1,5 +1,5 @@
-// This script here detects is a user is joining or leaving then sends a message 
-// into a channel. You will get a error saying Object is possibly 'null'. Just 
+// This script here detects is a user is joining or leaving then sends a message
+// into a channel. You will get a error saying Object is possibly 'null'. Just
 // ignore it!
 
 discord.on('GUILD_MEMBER_ADD', async (member) => {
@@ -13,7 +13,8 @@ discord.on('GUILD_MEMBER_ADD', async (member) => {
 
 discord.on('GUILD_MEMBER_REMOVE', async (member) => {
   const channelId = await discord.getTextChannel('971795641409888326');
+  let full = member.user.username + '#' + member.user.discriminator;
   await channelId.sendMessage(
-    '📤 **' + member.user.username + '** has left the server. Imagine.'
+    '📤 **' + full + '** has left the server. Imagine.'
   );
 });
